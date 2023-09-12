bc550b9b2c2a36f929806059c0e1bac1910a63c1
Hyuna-Kim0317 <november27blue@gmail.com>
2023-09-12 15:15:27 +0900

    lect 8

--- src/main/java/org/sp/news/controller/NewsController.java
+++ src/main/java/org/sp/news/controller/NewsController.java
@@ -1,5 +1,8 @@
 package org.sp.news.controller;
 
+import org.sp.news.domain.News;
+import org.sp.news.model.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +10,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class NewsController {
+	
+	@Autowired
+	private NewsService newsService;
+	
 	@GetMapping("/news/list")
 	public ModelAndView getList() {
 		
@@ -22,11 +29,14 @@ public class NewsController {
 	
 	//글등록
 	@PostMapping("/news/regist")
-	public ModelAndView regist() {
+	public ModelAndView regist(News news) {
 		//3단계: 오라클에 넣기
+		newsService.insert(news);
 		
 		//4단계: 저장X redirect
 		ModelAndView mav = new ModelAndView("redirect:/news/list");
 		return mav;
 	}
+	
+	
 }
